65499b38bc4aa30e08e2282a299c80dac17f43ae
Reema Barhoum <reemabarhoum07@gmail.com>
2026-06-07 10:25:52 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import Checkout from "./pages/Checkout";
 import Navbar from "./componentes/Navbar";
+import AuthProvider from './context/AuthContext';
 
 function App() {
 
 
   return (
+    <AuthProvider>
    <div className='app'>
     <Navbar/>
     <Routes>
@@ -21,6 +23,7 @@ function App() {
     </Routes>
 
    </div> 
+   </AuthProvider>
   );
 }
 
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,9 +1,97 @@
-
-
-
-
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 export default function Auth() {
-    return(
-       <div>AuthPage</div> 
-    )
+  const [mode, setMode] = useState("signup");
+  const {
+    register ,
+    handleSubmit ,
+    formState: {errors }
+  }= useForm();
+  function onSubmit() {
+    alert("sign up")
+  }
+
+
+  return (
+    <div className="page">
+      <div className="container">
+        <div className="auth-container">
+          <h1 className="page-title">
+            {mode === "signup" ? "Sign Up" : "Login"}
+          </h1>
+
+          <form className="auth-form" onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-group">
+              <label className="form-label" htmlFor="email">
+                Email
+              </label>
+
+              <input
+                className="form-input"
+                type="email"
+                id="email" {...register("email" , {required :"Eamil is required"}
+
+                )}
+              />
+              {errors.email && <span className="form-error"> {errors.email.message}
+                </span>}
+            </div>
+
+            <div className="form-group">
+              <label className="form-label" htmlFor="password">
+                Password
+              </label>
+
+              <input
+              {...register("password" , {required :"password is required" 
+                  , minLength :{
+                        value :6,
+                        message: "  password most be at least 6 charcter"
+                    },
+                    maxLength :{
+                        value :12,
+                        message: "  password most be leas than 12 charcter"
+                    },
+
+              })}
+                className="form-input"
+                type="password"
+                id="password"
+              />
+               {errors.password && (
+  <span className="form-error">
+    {errors.password.message}
+  </span>
+)}
+            </div>
+
+            <button
+              type="submit"
+              className="btn btn-primary btn-large"
+            >
+              {mode === "signup" ? "Sign Up" : "Login"}
+            </button>
+          </form>
+
+          <div className="auth-switch">
+            {mode === "signup" ? (
+              <p>
+                Already have an account?{" "}
+                <span className="auth-link" onClick={() =>setMode("login")}>
+                  Login
+                </span>
+              </p>
+            ) : (
+              <p>
+                Don't have an account?{" "}
+                <span className="auth-link" onClick={() =>setMode("signup")}>
+                  Sign Up
+                </span>
+              </p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
